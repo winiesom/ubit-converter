2f1b7f16ada29133446ecf218ef6ddd40d89e7e8
mmaesom <winifred.esomonu@stetis.com>
2023-11-19 21:58:02 +0000

    folder set up

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Node.js!');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
